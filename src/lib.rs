6c58a2c1cfad2a354af7280ec2965afad58e3b82
Omer Shlomovits <omer.shlomovits@gmail.com>
2020-04-28 23:48:12 +0300

    debug

--- src/lib.rs
+++ src/lib.rs
@@ -285,7 +285,10 @@ pub trait PolynomialCommitment<F: Field>: Sized {
                 comms.push(commitment);
                 values.push(*v_i);
             }
-println!("POINT");
+            println!("comms.len {:?}", comms.len());
+            println!("values.len {:?}", values.len());
+
+            println!("POINT");
             let proof_time = start_timer!(|| "Checking per-query proof");
             result &= Self::check(vk, comms, *query, values, &proof, opening_challenge)?;
             end_timer!(proof_time);
